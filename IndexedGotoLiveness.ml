a12dce53b4ad972f87ad32cd49a595522dbe0df4
Antonin Gallot <agallot@a-018818.ups.u-psud.fr>
2018-12-04 15:02:26 +0100

    J'avais pas save mdr

--- IndexedGotoLiveness.ml
+++ IndexedGotoLiveness.ml
@@ -5,15 +5,11 @@ type succ_table = int list array
 type liveness_info = { live_in: string list array;
                        live_out: string list array }
   
-type succ =
-    None
-  | Some of int
-  
 let rec find_succ_goto l i =
   match i with
   | n, Sequence(i1, i2) -> find_succ_goto l i1; find_succ_goto l i2
-  | n, Label(lbl) -> if lbl = l then Some n
-  | n, _ -> None
+  | n, Label(lbl) -> if lbl = l then n
+  | n, _ -> 
     
 let find_succ current_instr root_instr =
   match current_instr with
